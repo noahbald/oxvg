0a3009d2a688320b0769771878c6d9d4a49cdfbb
Noah Baldwin <noahwbaldwin@gmail.com>
2024-10-04 16:05:03 +1000

    release(oxvg_path): 0.0.1

--- crates/oxvg_ast/src/document.rs
+++
@@ -1,9 +0,0 @@
-use oxvg_diagnostics::SVGError;
-
-// [2.1 Well-Formed XML Documents](https://www.w3.org/TR/2006/REC-xml11-20060816/#sec-well-formed)
-
-#[derive(Debug)]
-pub struct Document {
-    /// A list of any errors encountered while parsing the document
-    pub errors: Vec<SVGError>,
-}
--- crates/oxvg_ast/src/lib.rs
+++ crates/oxvg_ast/src/lib.rs
@@ -1,8 +1,6 @@
 #[macro_use]
 extern crate markup5ever;
 
-mod document;
 pub mod node;
 
-pub use crate::document::Document;
 pub use crate::node::Attributes;
