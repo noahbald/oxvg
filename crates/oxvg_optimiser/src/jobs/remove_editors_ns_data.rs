9ef4f24ff750f446bab21f91faf2beba96d96e1a
Noah Baldwin <noahwbaldwin@gmail.com>
2024-11-29 08:52:55 +1100

    feat(oxvg_optimiser): #25 remove editor ns data

---
+++ crates/oxvg_optimiser/src/jobs/remove_editors_ns_data.rs
@@ -0,0 +1,148 @@
+use std::{collections::HashSet, marker::PhantomData};
+
+use oxvg_ast::{
+    attribute::{Attr, Attributes},
+    element::Element,
+    name::Name,
+    visitor::{Context, Visitor},
+};
+use oxvg_collections::collections::EDITOR_NAMESPACES;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Clone, Default, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct RemoveEditorsNSData {
+    additional_namespaces: Option<HashSet<String>>,
+}
+
+struct State<'o, 'arena, E: Element<'arena>> {
+    options: &'o RemoveEditorsNSData,
+    prefixes: HashSet<<E::Name as Name>::Prefix>,
+    marker: PhantomData<&'arena ()>,
+}
+
+impl<'arena, E: Element<'arena>> Visitor<'arena, E> for RemoveEditorsNSData {
+    type Error = String;
+
+    fn document(
+        &mut self,
+        document: &mut E,
+        context: &Context<'arena, '_, '_, E>,
+    ) -> Result<(), Self::Error> {
+        State {
+            options: &*self,
+            prefixes: HashSet::new(),
+            marker: PhantomData,
+        }
+        .start(document, context.info)
+        .map(|_| ())
+    }
+}
+
+impl<'arena, E: Element<'arena>> Visitor<'arena, E> for State<'_, 'arena, E> {
+    type Error = String;
+
+    fn document(
+        &mut self,
+        document: &mut E,
+        _context: &Context<'arena, '_, '_, E>,
+    ) -> Result<(), Self::Error> {
+        document.child_elements_iter().for_each(|ref e| {
+            self.collect_svg_namespace(e);
+        });
+        Ok(())
+    }
+
+    fn element(
+        &mut self,
+        element: &mut E,
+        _context: &mut Context<'arena, '_, '_, E>,
+    ) -> Result<(), Self::Error> {
+        self.remove_editor_attributes(element);
+        self.remove_editor_element(element);
+        Ok(())
+    }
+}
+
+impl<'arena, E: Element<'arena>> State<'_, 'arena, E> {
+    fn collect_svg_namespace(&mut self, element: &E) {
+        if element.local_name().as_ref() != "svg" {
+            return;
+        }
+
+        element.attributes().retain(|a| {
+            if a.prefix().as_ref().is_none_or(|p| p.as_ref() != "xmlns") {
+                return true;
+            }
+            let value = a.value().as_ref();
+            if !EDITOR_NAMESPACES.contains(value)
+                && !self
+                    .options
+                    .additional_namespaces
+                    .as_ref()
+                    .is_some_and(|set| set.contains(value))
+            {
+                return true;
+            }
+
+            let name = a.local_name();
+            log::debug!("Adding {name} to prefixes");
+            self.prefixes.insert(name.as_ref().into());
+            false
+        });
+    }
+
+    fn remove_editor_attributes(&mut self, element: &E) {
+        element.attributes().retain(|attr| {
+            let Some(prefix) = attr.prefix() else {
+                return true;
+            };
+
+            !self.prefixes.contains(prefix)
+        });
+    }
+
+    fn remove_editor_element(&mut self, element: &E) {
+        let Some(prefix) = element.prefix() else {
+            return;
+        };
+
+        if self.prefixes.contains(prefix) {
+            log::debug!("Removing element with prefix: {prefix}");
+            element.remove();
+        }
+    }
+}
+
+#[test]
+fn remove_editors_ns_data() -> anyhow::Result<()> {
+    use crate::test_config;
+
+    insta::assert_snapshot!(test_config(
+        r#"{ "removeEditorsNsData": {} }"#,
+        Some(
+            r#"<svg xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd">
+    <sodipodi:namedview>
+        ...
+    </sodipodi:namedview>
+
+    <path d="..." sodipodi:nodetypes="cccc"/>
+</svg>"#
+        ),
+    )?);
+
+    insta::assert_snapshot!(test_config(
+        r#"{ "removeEditorsNsData": {} }"#,
+        Some(
+            r#"<svg xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://inkscape.sourceforge.net/DTD/sodipodi-0.dtd">
+    <sodipodi:namedview>
+        ...
+    </sodipodi:namedview>
+
+    <path d="..." sodipodi:nodetypes="cccc"/>
+</svg>"#
+        ),
+    )?);
+
+    Ok(())
+}
