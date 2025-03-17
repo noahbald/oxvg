a1222affbeeebb060ef48ceb5e611703df368527
Noah Baldwin <noahwbaldwin@gmail.com>
2025-03-17 20:10:38 +1100

    feat(oxvg_optimiser): #119 port svgcleaner prechecks

--- crates/oxvg_optimiser/src/jobs/precheck.rs
+++ crates/oxvg_optimiser/src/jobs/precheck.rs
@@ -5,6 +5,7 @@ use oxvg_ast::{
     node::{self, Node},
     visitor::Visitor,
 };
+use oxvg_collections::collections::{ANIMATION_EVENT, DOCUMENT_EVENT, GRAPHICAL_EVENT};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug, Clone, Default)]
@@ -14,6 +15,10 @@ use serde::{Deserialize, Serialize};
 pub struct Precheck {
     /// Whether to exit with an error instead of a log
     fail_fast: bool,
+    /// Whether to run thorough pre-clean checks as to maintain document correctness
+    /// similar to [svgcleaner](https://github.com/RazrFalcon/svgcleaner)
+    #[serde(default = "default_preclean_check")]
+    preclean_checks: bool,
 }
 
 impl<E: Element> Visitor<E> for Precheck {
@@ -47,6 +52,13 @@ impl<E: Element> Visitor<E> for Precheck {
 
         for attr in element.attributes().into_iter() {
             self.qual_name::<E>(attr.name())?;
+        }
+
+        if self.preclean_checks {
+            self.check_for_unsupported_elements(element)?;
+            self.check_for_script_attributes(element)?;
+            self.check_for_conditional_attributes(element)?;
+            self.check_for_external_xlink(element)?;
         }
 
         Ok(())
@@ -86,6 +98,109 @@ impl Precheck {
     }
 }
 
+// NOTE: The following ports code from svgcleaner and thus this inherits the license
+// https://github.com/RazrFalcon/svgcleaner/blob/master/LICENSE.txt
+impl Precheck {
+    const SCRIPTING_NOT_SUPPORTED: &str = "scripting is not supported";
+    const ANIMATION_NOT_SUPPORTED: &str = "animation is not supported";
+    const CONDITIONAL_NOT_SUPPORTED: &str = "conditional processing attributes is not supported";
+
+    fn check_for_unsupported_elements<E: Element>(
+        &self,
+        element: &E,
+    ) -> Result<(), <Self as Visitor<E>>::Error> {
+        if element.prefix().is_some() {
+            return Ok(());
+        }
+
+        match element.local_name().as_ref() {
+            "script" => self.emit::<E>(Self::SCRIPTING_NOT_SUPPORTED),
+            "animate" | "animateColor" | "animateMotion" | "animateTransform" | "set" => {
+                self.emit::<E>(Self::ANIMATION_NOT_SUPPORTED)
+            }
+            _ => Ok(()),
+        }
+    }
+
+    fn check_for_script_attributes<E: Element>(
+        &self,
+        element: &E,
+    ) -> Result<(), <Self as Visitor<E>>::Error> {
+        for attr in element.attributes().into_iter() {
+            if attr.name().prefix().is_some() {
+                continue;
+            }
+
+            let local_name = attr.name().local_name();
+            if GRAPHICAL_EVENT.contains(local_name)
+                || DOCUMENT_EVENT.contains(local_name)
+                || ANIMATION_EVENT.contains(local_name)
+            {
+                self.emit::<E>(Self::SCRIPTING_NOT_SUPPORTED)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn check_for_conditional_attributes<E: Element>(
+        &self,
+        element: &E,
+    ) -> Result<(), <Self as Visitor<E>>::Error> {
+        for attr in element.attributes().into_iter() {
+            if attr.name().prefix().is_some() {
+                continue;
+            }
+            if attr.value().is_empty() {
+                continue;
+            }
+
+            match attr.name().local_name().as_ref() {
+                "requiredFeatures" | "systemLanguage" => {
+                    self.emit::<E>(Self::CONDITIONAL_NOT_SUPPORTED)?;
+                }
+                _ => {}
+            }
+        }
+
+        Ok(())
+    }
+
+    fn check_for_external_xlink<E: Element>(
+        &self,
+        element: &E,
+    ) -> Result<(), <Self as Visitor<E>>::Error> {
+        if matches!(
+            element.local_name().as_ref(),
+            "a" | "image" | "font-face-uri" | "feImage"
+        ) {
+            return Ok(());
+        }
+
+        for attr in element.attributes().into_iter() {
+            if attr
+                .name()
+                .prefix()
+                .as_ref()
+                .is_none_or(|p| p.as_ref() != "xlink")
+            {
+                continue;
+            }
+            if attr.name().local_name().as_ref() != "href" {
+                continue;
+            }
+
+            self.emit::<E>(&format!("the `xlink:href` attribute is referencing an external object '{}' which is not supported", attr.value()))?;
+        }
+
+        Ok(())
+    }
+}
+
+const fn default_preclean_check() -> bool {
+    false
+}
+
 #[test]
 fn precheck() {
     use crate::test_config;
@@ -118,5 +233,69 @@ fn precheck() {
             ),
         ).unwrap_err().to_string(),
         Precheck::NS_UNSTABLE_ERROR
+    );
+
+    assert_eq!(
+        &test_config(
+            r#"{ "precheck": { "failFast": true, "precleanChecks": true } }"#,
+            Some(
+            r##"<svg width="379px" height="134px" viewBox="0 0 379 134" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <!-- emit error for animation element -->
+    <circle id="1" cx="5.5" cy="5.5" r="5.5">
+        <animate attributeName="fill" calcMode="discrete" values="#6ebe28;#D8D8D8" dur="5s" keyTimes="0;0.15" repeatCount="indefinite"/>
+    </circle>
+</svg>"##,
+            ),
+        ).unwrap_err().to_string(),
+        Precheck::ANIMATION_NOT_SUPPORTED
+    );
+
+    assert_eq!(
+        &test_config(
+            r#"{ "precheck": { "failFast": true, "precleanChecks": true } }"#,
+            Some(
+            r#"<svg width="379px" height="134px" viewBox="0 0 379 134" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <!-- emit error for script attribute -->
+    <circle id="1" cx="5.5" cy="5.5" r="5.5" onerror="function (error) { console.log(error) }" />
+</svg>"#,
+            ),
+        ).unwrap_err().to_string(),
+        Precheck::SCRIPTING_NOT_SUPPORTED
+    );
+
+    assert_eq!(
+        &test_config(
+            r#"{ "precheck": { "failFast": true, "precleanChecks": true } }"#,
+            Some(
+            r#"<svg width="379px" height="134px" viewBox="0 0 379 134" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <!-- emit error for script attribute -->
+    <circle id="1" cx="5.5" cy="5.5" r="5.5" requiredFeatures="http://www.w3.org/TR/SVG11/feature#SVG" />
+</svg>"#,
+            ),
+        ).unwrap_err().to_string(),
+        Precheck::CONDITIONAL_NOT_SUPPORTED
+    );
+
+    let _ = test_config(
+        r#"{ "precheck": { "failFast": true, "precleanChecks": true } }"#,
+        Some(
+        r#"<svg width="379px" height="134px" viewBox="0 0 379 134" version="1.1" xmlns="http://www.w3.org/2000/svg">
+<!-- emit error for script attribute -->
+<circle id="1" cx="5.5" cy="5.5" r="5.5" requiredFeatures="" />
+</svg>"#,
+        ),
+    ).unwrap();
+
+    assert_eq!(
+        &test_config(
+            r#"{ "precheck": { "failFast": true, "precleanChecks": true } }"#,
+            Some(
+            r##"<svg width="379px" height="134px" viewBox="0 0 379 134" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- emit error for script attribute -->
+    <a xlink:href="#uwu" />
+</svg>"##,
+            ),
+        ).unwrap_err().to_string(),
+        "the `xlink:href` attribute is referencing an external object '#uwu' which is not supported"
     );
 }
