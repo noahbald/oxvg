c6b45b1098ef0a14a528668f6c2c7a449ceb228a
Noah Baldwin <noahwbaldwin@gmail.com>
2025-10-28 07:25:13 +1100

    chore: #179 rebuild napi types, update tests

--- packages/napi/test.js
+++ packages/napi/test.js
@@ -3,9 +3,6 @@ const assert = require("node:assert");
 
 const {
 	optimise,
-	Extends,
-	RemoveAttrs,
-	PreservePattern,
 	convertSvgoConfig,
 	extend,
 } = require("./index.js");
@@ -27,7 +24,7 @@ test("optimise basic svg", () => {
 	assert.equal(result, `<svg xmlns="http://www.w3.org/2000/svg"/>`);
 });
 
-test("optimise with config", () => {
+test("optimise with config", async () => {
 	const result = optimise(
 		`<svg xmlns="http://www.w3.org/2000/svg">
     <!-- Should convert to currentColor -->
@@ -46,77 +43,21 @@ test("optimise with config", () => {
 		result,
 		`<svg xmlns="http://www.w3.org/2000/svg"><!-- Should convert to currentColor --><g color="currentColor"/><g color="currentColor"/><g color="none"/><path fill="currentColor"/><path fill="currentColor"/><path fill="currentColor"/><path fill="none"/></svg>`,
 	);
-});
 
-test.describe("options requiring constructors", () => {
-	// FIXME: callback times out
-	test.skip("prefixIds", () => {
-		/** @param info {import("./index.js").PrefixGeneratorInfo} */
-		const generator = (info) => {
-			console.log({ info });
-			return info?.name;
-		};
-		const result = optimise(
-			`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1">
-	<rect id="id" x="10" y="10" width="100" height="100" />
-</svg>`,
-			{
-				prefixIds: {
-					delim: "-",
-					prefixIds: true,
-					prefixClassNames: false,
-					prefix: {
-						type: "Generator",
-						field0: generator,
-					},
-				},
-			},
-		);
-
-		assert.equal(
-			result,
-			`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1"><rect id="id" x="10" y="10" width="100" height="100"/></svg>`,
-		);
-	});
-
-	test("removeAttrs", () => {
-		const result = optimise(
-			`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1">
-	<path fill="red" d=""/>
-</svg>`,
-			{
-				removeAttrs: new RemoveAttrs(["path:fill"], ":", true),
-			},
-		);
-
-		assert.equal(
-			result,
-			`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1 1"><path d=""/></svg>`,
-		);
-	});
-
-	test("removeComments", () => {
-		const result = optimise(`<svg><!-- foo --><!-- bar --></svg>`, {
-			removeComments: { preservePatterns: [new PreservePattern("^\\s+foo")] },
-		});
-
-		assert.equal(result, `<svg><!-- foo --></svg>`);
-	});
-
-	describe("convertSvgoConfig", () => {
+	await describe("convertSvgoConfig", () => {
 		test("nullish falls back to default", () => {
 			const result = convertSvgoConfig();
-			assert.deepEqual(result, extend(Extends.Default));
+			assert.deepEqual(result, extend({ type: "Default" }));
 		});
 
 		test("empty", () => {
 			const result = convertSvgoConfig([]);
-			assert.deepEqual(result, extend(Extends.None));
+			assert.deepEqual(result, extend({ type: "None" }));
 		});
 
 		test("without parameters", () => {
 			const result = convertSvgoConfig([{ name: "inlineStyles" }]);
-			/** @type {import("./index.js").Jobs} */
+			/** @type {import("./index.d.ts").Jobs} */
 			const expected = {
 				inlineStyles: {
 					onlyMatchedOnce: true,
@@ -137,7 +78,7 @@ test.describe("options requiring constructors", () => {
 					},
 				},
 			]);
-			/** @type {import("./index.js").Jobs} */
+			/** @type {import("./index.d.ts").Jobs} */
 			const expected = {
 				inlineStyles: {
 					onlyMatchedOnce: false,
@@ -150,3 +91,4 @@ test.describe("options requiring constructors", () => {
 		});
 	});
 });
+
