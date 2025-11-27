d285386af7afb2e009a8665ab39b7c887d73681c
Noah Baldwin <noahwbaldwin@gmail.com>
2025-11-27 17:13:59 +1100

    feat: create reusable directory walker

--- crates/oxvg/src/commands/format.rs
+++ crates/oxvg/src/commands/format.rs
@@ -1,9 +1,16 @@
 use std::path::PathBuf;
 
-use oxvg_ast::xmlwriter::{Indent, Space};
-use oxvg_optimiser::{Extends, Jobs};
+use oxvg_ast::{
+    parse::roxmltree::parse_with_options,
+    xmlwriter::{Indent, Options, Space},
+};
+use roxmltree::ParsingOptions;
 
-use crate::{args::RunCommand, commands::Optimise, config::Config};
+use crate::{
+    args::RunCommand,
+    config::Config,
+    walk::{Output, Walk},
+};
 
 #[derive(clap::Args, Debug)]
 /// Runs [`Optimise`] with options and defaults specialised for formatting
@@ -13,10 +20,10 @@ pub struct Format {
     pub paths: Vec<PathBuf>,
     /// Whether to write to the specified file or directory.
     /// Will use the input if flag is given without a value.
-    /// Defaults to stdout.
+    /// Defaults to standard output.
     #[clap(long, short, num_args(0..=1))]
     pub output: Option<Vec<PathBuf>>,
-    /// If the path is a directory, whether to walk through and optimise it's subdirectories
+    /// If the path is a directory, whether to walk through and optimise its subdirectories
     #[clap(long, short, default_value = "false")]
     pub recursive: bool,
     /// Search through hidden files and directories
@@ -35,17 +42,44 @@ pub struct Format {
 
 impl RunCommand for Format {
     fn run(self, _config: Config) -> anyhow::Result<()> {
-        let optimise = Optimise {
-            paths: self.paths,
-            output: self.output,
-            config: None,
+        let walk = Walk {
+            paths: &self.paths,
+            output: self.output.as_ref().and_then(|output| output.first()),
             recursive: self.recursive,
             hidden: self.hidden,
             threads: self.threads,
-            extends: Some(Extends::None),
-            pretty: self.pretty,
-            space: self.space,
         };
-        optimise.handle_paths(&Jobs::none())
+        walk.run(|| {
+            let format_options = Options {
+                indent: self.pretty,
+                trim_whitespace: self.space,
+                ..Options::default()
+            };
+            Box::new(move |source, path, output| {
+                let result = parse_with_options(
+                    source,
+                    ParsingOptions {
+                        allow_dtd: true,
+                        ..ParsingOptions::default()
+                    },
+                    |dom, _| -> anyhow::Result<()> {
+                        let output = Output {
+                            options: format_options,
+                            dom,
+                            input: path,
+                            destination: output,
+                            input_size: source.len() as f64,
+                        };
+                        output.output()?;
+                        Ok(())
+                    },
+                );
+                match result {
+                    Err(err) => eprintln!("{err}"),
+                    Ok(Err(err)) => eprintln!("{err}"),
+                    Ok(Ok(())) => {}
+                }
+            })
+        })
     }
 }
